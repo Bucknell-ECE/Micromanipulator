7053919bc024e0e9333b4aa1205ff04aff2fb2f7
rrn006 <rrn006@bucknell.edu>
2017-11-09 11:58:02 -0500

    Changed calibration settings

--- testStage.py
+++ testStage.py
@@ -7,7 +7,7 @@ while True:
     moveToNew = input('Where should the stage move next?')
     #com = xaxis.buildCommand('08',encoderConvert(moveToNew))
     #print(com)
-    xaxis.calibrate()
+    #xaxis.calibrate()
     xaxis.sendCommand('08', encoderConvert(moveToNew))
     input('Finished, Continue?')
 
