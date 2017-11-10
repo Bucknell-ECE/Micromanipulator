64470ce3a7b72b2143651c82440387e008eaa242
rrn006 <rrn006@bucknell.edu>
2017-11-10 18:13:37 -0500

    Updated one set of build functions in Stage class to output decimal values

--- testStage.py
+++ testStage.py
@@ -11,7 +11,7 @@ while True:
     #print(com)
     #xaxis.calibrate()
     bus.write_i2c_block_data(0x32, 0, [100, 60, 48, 56, 32, 48, 48, 48, 48, 48, 51, 69, 56, 62, 13])
-    xaxis.sendCommand('08', encoderCountConvert(moveToNew))
+   # xaxis.sendCommand('08', encoderCountConvert(moveToNew))
     input('Finished, Continue?')
 
 
