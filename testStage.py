225ab056514fac3aa9467ddb4b87462b494fdde8
rrn006 <rrn006@bucknell.edu>
2017-11-10 15:49:22 -0500

    Updated one set of build functions in Stage class to output decimal values

--- testStage.py
+++ testStage.py
@@ -4,10 +4,13 @@ from Stage import *
 xaxis = Stage('0x32', 6000, 1)
 
 while True:
+    bus = smbus.SMBus(1)
+
     moveToNew = input('Where should the stage move next?')
     #com = xaxis.buildCommand('08',encoderConvert(moveToNew))
     #print(com)
     #xaxis.calibrate()
+    bus.write_i2c_block_data(32, 0, [64, 60, 48, 56, 32, 30, 30, 30, 30, 30, 66, 66, 56, 62, 13])
     xaxis.sendCommand('08', encoderCountConvert(moveToNew))
     input('Finished, Continue?')
 
