5e2bd6ff32c2379b6458c5190be5e00174540ecf
rrn006 <rrn006@bucknell.edu>
2017-12-16 12:42:46 -0500

    Updated one set of build functions in Stage class to output decimal values

---
+++ Joystick.py
@@ -0,0 +1,16 @@
+import pygame
+
+class CustomJoystick:
+
+    def __init__(self, name, numberaxes, numberbuttons):
+        self.name = name
+        self.numberaxes = numberaxes
+        self.numberbuttons = numberbuttons
+
+    def getAxisPosition(self, axisIndex):
+        return self.get_axis(axisIndex)
+
+    def convertPosition(self, axisIndex):
+        currPos = self.getAxisPosition(axisIndex)
+        newPos = (currPos + 1)*127.5
+        return newPos
