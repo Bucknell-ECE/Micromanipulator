ac47bb8cb9a17c4dffa2aa625d757dcf9e68cf33
rrn006 <rrn006@bucknell.edu>
2017-12-16 18:28:43 -0500

    Updated one set of build functions in Stage class to output decimal values

--- buttonHandlerT.py
+++ buttonHandlerT.py
@@ -1,14 +1,7 @@
 import pygame
 import time
 
-
 pygame.init()
-
-
-#done = False
-
-# Used to manage how fast the screen updates
-#clock = pygame.time.Clock()
 
 # Initialize the joysticks
 pygame.joystick.init()
@@ -17,33 +10,19 @@ joystick.init()
 
 button2count = 0
 # -------- Main Program Loop -----------
+
+
 while True:
     # EVENT PROCESSING STEP
+    input('Type a key when you would like a status update')
     for event in pygame.event.get():  # User did something
-       # if event.type == pygame.QUIT:  # If user clicked close
-          #  done = True  # Flag that we are done so we exit this loop
-
-        # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
-        #if event.type == pygame.JOYBUTTONDOWN:
-            #print("Joystick button pressed.")
-            #print(event)
         if event.type == pygame.JOYBUTTONUP:
-            #print("Joystick button released.")
-            #print(event)
-            #print(event.type)
-            #print(event.button)
             button = event.button
             print("Button {} off".format(button))
             #if button == 2:
                # button2count += 1
         #print('Button 2 Coutnt is : ', button2count)
 
-    time.sleep(2)
+    #time.sleep(2)
 
-    # Limit to 20 frames per second
- #   clock.tick(20)
-
-# Close the window and quit.
-# If you forget this line, the program will 'hang'
-# on exit if running from IDLE.
 #pygame.quit()
