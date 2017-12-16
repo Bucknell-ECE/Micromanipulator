c316236e77630b4d1131a438693580cc8cead074
rrn006 <rrn006@bucknell.edu>
2017-12-16 13:32:57 -0500

    Updated one set of build functions in Stage class to output decimal values

--- EventHandlerTesting.py
+++ EventHandlerTesting.py
@@ -1,11 +1,37 @@
 import pygame
 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
+clock = pygame.time.Clock()
+# This is a simple class that will help us print to the screen
+# It has nothing to do with the joysticks, just outputting the
+# information.
+class TextPrint:
+    def __init__(self):
+        self.reset()
+        self.font = pygame.font.Font(None, 20)
+
+    def printy(self, screen, textString):
+        textBitmap = self.font.render(textString, True, BLACK)
+        screen.blit(textBitmap, [self.x, self.y])
+        self.y += self.line_height
+
+    def reset(self):
+        self.x = 10
+        self.y = 10
+        self.line_height = 15
+
+    def indent(self):
+        self.x += 10
+
+    def unindent(self):
+        self.x -= 10
 pygame.init() # Initialize all pygame modules
 pygame.joystick.init() # Initialize joystick module
 size = [500, 700]
 screen = pygame.display.set_mode(size)
-
+textPrint = TextPrint()
 while True:
     for event in pygame.event.get():  # User did something
 
@@ -14,3 +40,10 @@ while True:
             print("Joystick button pressed.")
         if event.type == pygame.JOYBUTTONUP:
             print("Joystick button released.")
+
+    screen.fill(WHITE)
+    textPrint.reset()
+    pygame.display.flip()
+
+    # Limit to 20 frames per second
+    clock.tick(20)
