60f5c31c266731b2162c87e3e13ed8b1c104b9b8
rrn006 <rrn006@bucknell.edu>
2017-12-16 13:27:32 -0500

    Updated one set of build functions in Stage class to output decimal values

--- EventHandlerTesting.py
+++ EventHandlerTesting.py
@@ -3,11 +3,11 @@ import pygame
 
 pygame.init() # Initialize all pygame modules
 pygame.joystick.init() # Initialize joystick module
+while True:
+    for event in pygame.event.get():  # User did something
 
-for event in pygame.event.get():  # User did something
-
-    # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
-    if event.type == pygame.JOYBUTTONDOWN:
-        print("Joystick button pressed.")
-    if event.type == pygame.JOYBUTTONUP:
-        print("Joystick button released.")
+        # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
+        if event.type == pygame.JOYBUTTONDOWN:
+            print("Joystick button pressed.")
+        if event.type == pygame.JOYBUTTONUP:
+            print("Joystick button released.")
