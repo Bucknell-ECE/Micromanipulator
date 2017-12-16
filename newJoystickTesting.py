0fe761d6d7c24e2579ceddd4778d3fe561dbf080
rrn006 <rrn006@bucknell.edu>
2017-12-16 18:35:15 -0500

    Updated one set of build functions in Stage class to output decimal values

--- newJoystickTesting.py
+++ newJoystickTesting.py
@@ -21,29 +21,9 @@ def getButtons():
     print('events being called')
     commands = []
     for event in pygame.event.get():  # User did something
-        print(event)
-        if event.type == pygame.QUIT:  # If user clicked close
-            done = True  # Flag that we are done so we exit this loop
-
-        # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
-        if event.type == pygame.JOYBUTTONDOWN:
-            # print("Joystick button pressed.")
+        if event.type == pygame.JOYBUTTONUP:
             button = event.button
-            print('button num ', button)
-            print(buttonMap[button])
-            commands += buttonMap[button]
-            print(event)
-            # if event.type == pygame.JOYBUTTONUP:
-            # print("Joystick button released.")
-            # print(event)
-            # print(event.type)
-            # print(event.button)
-            # button = event.button
-            # print("Button {} off".format(button))
-            #  if button == 2:
-            #      button2count += 1
-        # print('Button 2 Coutnt is : ', button2count)
-        clock.tick(20)
+            print("Button {} off".format(button))
     return commands
 while True:
 
