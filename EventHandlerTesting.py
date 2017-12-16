3dd0c9e0723f741878161b5c1915701193248cc8
rrn006 <rrn006@bucknell.edu>
2017-12-16 14:02:38 -0500

    Updated one set of build functions in Stage class to output decimal values

--- EventHandlerTesting.py
+++ EventHandlerTesting.py
@@ -65,7 +65,7 @@ while done == False:
             print("Joystick button released.")
             print(event)
             print(event.type)
-            print(["button"])
+            print(event["button"])
 
     # DRAWING STEP
     # First, clear the screen to white. Don't put other drawing commands
