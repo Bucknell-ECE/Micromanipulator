9c202c40b744723bab52624a90419781c00c830c
rrn006 <rrn006@bucknell.edu>
2017-12-16 18:30:14 -0500

    Updated one set of build functions in Stage class to output decimal values

--- buttonHandlerT.py
+++ buttonHandlerT.py
@@ -14,15 +14,16 @@ button2count = 0
 
 while True:
     # EVENT PROCESSING STEP
-    input('Type a key when you would like a status update')
-    for event in pygame.event.get():  # User did something
-        if event.type == pygame.JOYBUTTONUP:
-            button = event.button
-            print("Button {} off".format(button))
-            #if button == 2:
-               # button2count += 1
-        #print('Button 2 Coutnt is : ', button2count)
+    test = input('Type a key when you would like a status update')
+    if test == 1:
+        for event in pygame.event.get():  # User did something
+            if event.type == pygame.JOYBUTTONUP:
+                button = event.button
+                print("Button {} off".format(button))
+                #if button == 2:
+                   # button2count += 1
+            #print('Button 2 Coutnt is : ', button2count)
 
-    #time.sleep(2)
+        #time.sleep(2)
 
 #pygame.quit()
