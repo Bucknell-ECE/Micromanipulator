6a68c1f635fdba4e480e46da6acc4e5ddd01c350
rrn006 <rrn006@bucknell.edu>
2017-12-16 13:51:21 -0500

    Updated one set of build functions in Stage class to output decimal values

--- EventHandlerTesting.py
+++ EventHandlerTesting.py
@@ -60,8 +60,10 @@ while done == False:
         # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
         if event.type == pygame.JOYBUTTONDOWN:
             print("Joystick button pressed.")
+            print(event)
         if event.type == pygame.JOYBUTTONUP:
             print("Joystick button released.")
+            print(event)
 
     # DRAWING STEP
     # First, clear the screen to white. Don't put other drawing commands
