3e1909dc5ac1e16a74c5fd18f93ebeb8070cdef0
rrn006 <rrn006@bucknell.edu>
2017-12-17 11:43:00 -0500

    Updated one set of build functions in Stage class to output decimal values

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from helper import *
 from Stage import *
 from datetime import datetime
+from Joystick import *
+import pygame
 xaxis = Stage(0x32, 6000, 1)
 yaxis = Stage(0x33, 6000, 1)
 zaxis = Stage(0x34, 6000, 1)
-controlMode = 'velocity'
+controlMode = 'position'
 xlinearRangeMin = 0
 xlinearRangeMax = 12000
 xlinearRange = 12000
@@ -14,9 +16,16 @@ ylinearRange = 12000
 constrainedLinearRange = 12000
 axes = [xaxis, yaxis, zaxis]
 home = [xaxis.home, yaxis.home, zaxis.home]
-locations = [xlocation, ylocation, zlocation]
+#locations = [xlocation, ylocation, zlocation]
 refreshRate = 200000
 lastMillis = 0
+
+pygame.init() # Initialize all pygame modules
+pygame.joystick.init() # Initialize joystick module
+
+
+joy = CustomJoystick('Logitech', 0)
+
 def setControlMode(newControlMode):
     controlMode = newControlMode
 
@@ -43,15 +52,15 @@ def setBounds():
     boundries = [home[0], home[1], 12000 - home[0], 12000 - home[1]]
     constrainedLinearRange = min(boundries)
 
-    scaleInput = joystick.getThrottle
+    scaleInput = joy.getThrottle()
 
-    scaledRange = mapval(scaleInput, 0, 255, 0, constrainedLinearRange)
+    scaledRange = mapval(scaleInput, 0, 1023, 0, constrainedLinearRange)
 
 
-    xlinearRangeMin = xaxis.home - scaledRange
-    xlinearRangeMax = xaxis.home + scaledRange
-    ylinearRangeMin = yaxis.home - scaledRange
-    ylinearRangeMax = yaxis.home + scaledRange
+    xlinearRangeMin = xaxis.home - scaledRange +100
+    xlinearRangeMax = xaxis.home + scaledRange -100
+    ylinearRangeMin = yaxis.home - scaledRange +100
+    ylinearRangeMax = yaxis.home + scaledRange -100
 
 
 while True:
@@ -61,13 +70,13 @@ while True:
     else:
 
         lastMillis = currentMillis
-        if controlMode == 'velocity':
-            fsdjfl
+        #if controlMode == 'velocity':
+            #fsdjfl
         if controlMode == 'position':
             setBounds()
 
-            xaxis.goToLocation(mapval(joy.getx(), 0, 255, xlinearRangeMin, xlinearRangeMax))
-            yaxis.goToLocation(mapval(joy.gety(), 0, 255, ylinearRangeMin, ylinearRangeMax))
+            xaxis.goToLocation(mapval(joy.getX(), 0, 1023, xlinearRangeMin, xlinearRangeMax))
+            #yaxis.goToLocation(mapval(joy.gety(), 0, 255, ylinearRangeMin, ylinearRangeMax))
 
 
 
