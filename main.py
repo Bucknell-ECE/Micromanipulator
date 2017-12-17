39a59fafb0e2c3a94e70a2cacab5bfc4a982683b
rrn006 <rrn006@bucknell.edu>
2017-12-17 13:43:00 -0500

    Updated one set of build functions in Stage class to output decimal values

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ while True:
         #print(time.time())
         #print(datetime.now())
         #time.sleep(0.0001)
-        time.sleep(0.05)
+        time.sleep(0.1)
     except KeyboardInterrupt:
         xaxis.sendCommandNoVars('19')
         temp = xaxis.bus.read_i2c_block_data(0x32, 0)
