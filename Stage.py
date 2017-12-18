d6066876a9eec74a2a2b26efd7140523fac34d97
rrn006 <rrn006@bucknell.edu>
2017-12-17 23:55:00 -0500

    Updated one set of build functions in Stage class to output decimal values

--- Stage.py
+++ Stage.py
@@ -84,7 +84,7 @@ class Stage:
         bus = smbus.SMBus(1)
         #bus.write_i2c_block_data(self.address, 0, command)
         ##############CHANGED TO 1 BUT SHOULD BE ZERO
-        bus.write_i2c_block_data(self.address, 1, command)
+        bus.write_i2c_block_data(self.address, 0, command)
 
     def write1(self, command):
         bus = smbus.SMBus(1)
