234ef2119ed6622a2fae2084c33cd6c8025473a1
rrn006 <rrn006@bucknell.edu>
2018-02-27 21:34:12 -0500

    Updated one set of build functions in Stage class to output decimal values

--- main.py
+++ main.py
@@ -89,12 +89,14 @@ while True:
         if len(buttons) != 0:
             for nums in range(buttons.count('Zup')):
                 print('Theres a ZUP')
-                zaxis.sendCommand('06 1', [48,48,48,48,48,48,55,56])
+                #zaxis.sendCommand('06 1', [48,48,48,48,48,48,55,56])
+                zaxis.zMove(0, 200)
                 #move up120 encoder counts
 
             for nums in range(buttons.count('Zdown')):
                 print('Theres a zdonw')
-                zaxis.sendCommand('06 0', [48, 48, 48, 48, 48, 48, 55, 56])
+                #zaxis.sendCommand('06 0', [48, 48, 48, 48, 48, 48, 55, 56])
+                zaxis.zMove(1,200)
 
                 #move down some amount 120 encoder counts
 
