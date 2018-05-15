a6273af0c6dbf1215574b16a4e3ea849539a6de7
rrn006 <rrn006@bucknell.edu>
2018-05-15 11:20:15 -0400

    Removeed X axis for Z axis testing

--- mainSpiTesting.py
+++ mainSpiTesting.py
@@ -14,7 +14,7 @@ import spidev
 #xaxis = StageSPI(0, 0, 6000)
 #yaxis = StageSPI(0, 1, 6000)
 zaxis = Stage(0x40, 6000, 1)
-xaxis.sendCommandNoVars('08')
+#xaxis.sendCommandNoVars('08')
 #yaxis = Stage(0x33, 6000, 1)
 #zaxis = Stage(0x34, 6000, 1)
 controlMode = 'position'
