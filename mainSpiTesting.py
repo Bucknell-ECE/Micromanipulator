833845d9f8de02db0e34c8343173a2a328882567
rrn006 <rrn006@bucknell.edu>
2018-05-16 18:14:12 -0400

    Neatened all code

--- mainSpiTesting.py
+++ mainSpiTesting.py
@@ -117,11 +117,11 @@ while True:
         # deal with the Z axis
 
 
-        print(scaledRange)
         home = [zaxis.home, yaxis.home, zaxis.home]
         boundries = [home[0], home[1], 12001 - home[0], 12001 - home[1]]
         constrainedLinearRange = min(boundries)
         scaledRange = mapval(scaleInput, 0, 100, 0, constrainedLinearRange)
+        print(scaledRange)
         xlinearRangeMin = home[0] - scaledRange + 100
         xlinearRangeMax = home[0] + scaledRange - 100
         print('XlinMin', xlinearRangeMin)
