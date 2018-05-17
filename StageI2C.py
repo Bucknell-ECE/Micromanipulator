74cbf34108a9862f9841e1aac73a9690d9822947
rrn006 <rrn006@bucknell.edu>
2018-05-16 23:02:37 -0400

    Bug Fixes

--- StageI2C.py
+++ StageI2C.py
@@ -12,7 +12,7 @@ class StageI2C(Stage):
         self.address = address
         self.bus = smbus.SMBus(bus)
         self.home = 6000
-    bus = smbus.SMBus(1)  # initialize the SMBus (I2C bus) on the pi. The bus we use is 1.
+    #bus = smbus.SMBus(1)  # initialize the SMBus (I2C bus) on the pi. The bus we use is 1.
 
     def zMove(self, direction, encoder_counts):
         """
@@ -30,17 +30,17 @@ class StageI2C(Stage):
         decimal values for each of the ascii characters in the command to be sent.
         :return:
         """
-        bus = smbus.SMBus(1)
+        #bus = smbus.SMBus(1)
         print(commandToString(command))  # print the command in  a user readable format.
-        bus.write_i2c_block_data(self.address, 0, command)
+        self.bus.write_i2c_block_data(self.address, 0, command)
 
     def read(self):
         """
         Reads from the output register of the stage
         :return: List of signed values that reprsent what is on the output register of the stage
         """
-        bus = self.bus
-        temp = bus.read_i2c_block_data(self.address, 0)
+        #bus = self.bus
+        temp = self.bus.read_i2c_block_data(self.address, 0)
         print('temp', temp)
         return_buffer = []
         for i in temp:
