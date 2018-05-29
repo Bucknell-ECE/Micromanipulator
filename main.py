250e31b7c2d2b11e094da2225db7e798edebde46
zt003 <zt003@bucknell.edu>
2018-05-29 14:19:34 -0400

    Testing Audio Notification

--- main.py
+++ main.py
@@ -149,6 +149,8 @@ while True:
                 yaxis.setHome(6000)
             for nums in range(buttons.count('GetStatus')):
                 print('Getstatus X', xaxis.getstatus())
+                print('Getstatus Y', yaxis.getstatus())
+                print('Getstatus Z', zaxis.getstatus())
             for nums in range(buttons.count('Z Sensitivity Up')):
                 print('Z sensitivity up by 50, Now the sensitivity is',Zsensitivity)
                 Zsensitivity += 50
