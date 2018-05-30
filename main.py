a89f972623d40e12728c85a4978972d0d8eb8737
zt003 <zt003@bucknell.edu>
2018-05-30 15:51:28 -0400

    Testing Audio Notification

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ def setBounds():
 while True:
 
     try:
-        print('start up test', xaxis.sendCommand('06',['0x30'] + ['0x20']))
+        print('start up test', xaxis.sendCommand('06',['48'] + ['32']))
         time.sleep(0.01)
         buttons = []
         buttons = joy.getButtons()
