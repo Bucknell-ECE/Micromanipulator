84ed27e67a39d9aef3cd4e0fb0fe51e677eaca9c
zt003 <zt003@bucknell.edu>
2018-05-30 15:52:12 -0400

    Testing Audio Notification

--- Stage.py
+++ Stage.py
@@ -96,8 +96,8 @@ class Stage(object):
         :return:
         """
         command_to_send = self.buildCommand(command_code, command_vars)
-        #return command_to_send
-        print(commandToString(command_to_send))
+        return command_to_send
+        #print(commandToString(command_to_send))
         #self.write(command_to_send)
 
 
