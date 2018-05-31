8c638802b9d70c09dd8f7f2ebfbb5266ce9257c8
zt003 <zt003@bucknell.edu>
2018-05-31 12:20:05 -0400

    Testing Startup Function

--- main.py
+++ main.py
@@ -114,7 +114,6 @@ def setBounds():
 
     # x_status = xaxis.getstatus()
     # z_status = zaxis.getstatus()
-
     # xaxis.MotorDirection(x_status)
     # zaxis.MotorDirection(z_status)
     # xaxis.Running(x_status)
@@ -126,7 +125,8 @@ while True:
     try:
         #print('xaxis location',xaxis.getPositionFromM3LS()), location in 12000
         #print('go to location test', xaxis.sendCommand('08', encodeToCommand(3000)))
-        #print('command test', xaxis.sendCommand('06', [48] + [32] + encodeToCommand(100)))  Test result: <06 0 00000064>\r
+        #print('command test', xaxis.sendCommand('06', [48] + [32] + encodeToCommand(100)))
+        # Test result: <06 0 00000064>\r
         time.sleep(0.01)
         buttons = []
         buttons = joy.getButtons()
