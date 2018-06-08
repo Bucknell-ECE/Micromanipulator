ee968e9facc1be5011ac1a9b81206951073725cf
zt003 <zt003@bucknell.edu>
2018-06-08 15:24:03 -0400

    Testing Startup Function

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ while True:
         print(buttons)
         X = mapval(x, 0, 2000, xlinearRangeMin, xlinearRangeMax)
         Y = mapval(y, 0, 2000, ylinearRangeMin, ylinearRangeMax)
-        # AudioNoti(X,Y,xlinearRangeMin,xlinearRangeMax,ylinearRangeMin,ylinearRangeMax)
+        AudioNoti(X,Y,xlinearRangeMin,xlinearRangeMax,ylinearRangeMin,ylinearRangeMax)
         # print('Getstatus X', xaxis.getstatus())
         # print('Getstatus Z', zaxis.getstatus())
         if len(buttons) != 0:
