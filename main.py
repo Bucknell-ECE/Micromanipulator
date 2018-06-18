495d9c85458c1f9b1058f0a7a24adcae7c1a283c
zt003 <zt003@bucknell.edu>
2018-06-18 11:21:18 -0400

    Testing Startup Function

--- main.py
+++ main.py
@@ -228,56 +228,56 @@ def main():
 #     main()
 
 #Refreshing Rate of 0.05s
-# def exitTK():
-#     global root
-#     root.destroy()
-#
-# while True:
-#     root=Tk()
-#     main()
-#     positionx = Label(root, text = ('Position x is ', x))
-#     positionx.pack()
-#     positiony = Label(root, text = ('Position y is ', y))
-#     positiony.pack()
-#     root.after(50,exitTK)
-#     root.mainloop()
-
-
-root = Tk(className = 'Micromanipulator')
-positionx = Label(root, text = "welcome")
-positiony = Label(root, text = "welcome")
-sensitivity_scale = Label(root, text = "welcome")
-exit = Button(root, text = "Quit", command = quit)
-statusx = Label(root, text = 'welcome')
-statusy = Label(root, text = 'welcome')
-statusz = Label(root, text = 'welcome')
-positionx.pack()
-positiony.pack()
-sensitivity_scale.pack()
-statusx.pack()
-statusy.pack()
-statusz.pack()
-exit.pack()
-
-def quit():
+def exitTK():
     global root
-    root.quit()
+    root.destroy()
 
 while True:
-    root.update()
+    root=Tk()
     main()
-    positionx['text'] = ('Position x is ',x)
+    positionx = Label(root, text = ('Position x is ', x))
     positionx.pack()
-    positiony['text'] = ('Position y is ',y)
+    positiony = Label(root, text = ('Position y is ', y))
     positiony.pack()
-    sensitivity_scale['text'] = ('Sensitivity Level is ', scaleInput)
-    sensitivity_scale.pack()
-    statusx['text'] = ('x status is ', xstatus)
-    statusx.pack()
-    statusy['text'] = ('y status is ', ystatus)
-    statusy.pack()
-    statusz['text'] = ('z status is ', zstatus)
-    statusz.pack()
+    root.after(50,exitTK)
+    root.mainloop()
+
+
+# root = Tk(className = 'Micromanipulator')
+# positionx = Label(root, text = "welcome")
+# positiony = Label(root, text = "welcome")
+# sensitivity_scale = Label(root, text = "welcome")
+# exit = Button(root, text = "Quit", command = quit)
+# statusx = Label(root, text = 'welcome')
+# statusy = Label(root, text = 'welcome')
+# statusz = Label(root, text = 'welcome')
+# positionx.pack()
+# positiony.pack()
+# sensitivity_scale.pack()
+# statusx.pack()
+# statusy.pack()
+# statusz.pack()
+# exit.pack()
+#
+# def quit():
+#     global root
+#     root.quit()
+#
+# while True:
+#     root.update()
+#     main()
+#     positionx['text'] = ('Position x is ',x)
+#     positionx.pack()
+#     positiony['text'] = ('Position y is ',y)
+#     positiony.pack()
+#     sensitivity_scale['text'] = ('Sensitivity Level is ', scaleInput)
+#     sensitivity_scale.pack()
+#     statusx['text'] = ('x status is ', xstatus)
+#     statusx.pack()
+#     statusy['text'] = ('y status is ', ystatus)
+#     statusy.pack()
+#     statusz['text'] = ('z status is ', zstatus)
+#     statusz.pack()
 
 
 '''
