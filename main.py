aa9b9dc01a7b863fdfe79d5b0f07871545be4be6
zt003 <zt003@bucknell.edu>
2018-06-29 12:26:40 -0400

    changing speed to 1024 um/s

--- main.py
+++ main.py
@@ -251,9 +251,11 @@ def main():
         # print('mapval y ', mapval(y, 0, 2000, ylinearRangeMin, ylinearRangeMax))
 
         # Move Open Loop Steps
-        xaxis.sendCommand('05', [48] + [32] + encodeToCommand4digit(1000)+[32]+hextocommand4('186A')+[32]+hextocommand4('0C35'))
-        time.sleep(3)
-        xaxis.sendCommand('05', [49] + [32] + encodeToCommand4digit(1000)+[32]+hextocommand4('186A')+[32]+hextocommand4('0C35'))
+        xaxis.sendCommand('05', [48] + [32] + encodeToCommand4digit(10)+[32]+hextocommand4('186A')+[32]+hextocommand4('0C35'))
+        time.sleep(0.2)
+        temp2 = xaxis.read()
+        print("This is feedback",temp2)
+        #xaxis.sendCommand('05', [49] + [32] + encodeToCommand4digit(1000)+[32]+hextocommand4('186A')+[32]+hextocommand4('0C35'))
 
         # root=Tk()
         # positionx = Label(root, text = ('Postion x is ',x))
