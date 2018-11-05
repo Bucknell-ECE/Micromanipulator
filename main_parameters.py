c792a8708a5aac9484eca53de9edc4abace76325
Jacquelyn Scott <jb.scott55@gmail.com>
2018-11-04 19:27:02 -0500

    Neatened code, copied over from failed push from linuxremote.

--- main_parameters.py
+++ main_parameters.py
@@ -9,11 +9,15 @@ xlinearRange = 12000
 ylinearRangeMin = 0
 ylinearRangeMax = 12000
 ylinearRange = 12000
+
 constrainedLinearRange = 12000
+
 sensitivity = 50
 Zsensitivity = 200
+
 getstatus = 0
 scaleInput = 0
+
 x_status = ''
 y_status = ''
 z_status = ''
