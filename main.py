ff31f9405da79ff0a8feb854aa19bcd801434b01
Jacquelyn Scott <jb.scott55@gmail.com>
2018-12-14 00:46:12 -0500

    ˆ

--- main.py
+++ main.py
@@ -202,7 +202,7 @@ def main():
         # x_axis.send_command_no_vars('19')
         # temp = x_axis.bus.read_i2c_block_data(0x33, 0)
         f1.close()
-        # raise
+        raise
 
 
 start_time = time.time()
