55d28929dee57ef042cf2b13dba65cd56bf5e19b
Jacquelyn Scott <jbs044@bucknell.edu>
2019-04-01 13:38:06 -0400

    Simplified and further neatened helper functions and Joystick class functions.

--- main.py
+++ main.py
@@ -161,11 +161,11 @@ def main():
                 joy.increase_scale_factor()
 
 
-            except KeyboardInterrupt:
+            # except KeyboardInterrupt:
                 # x_axis.send_command_no_vars('19')
                 # temp = x_axis.bus.read_i2c_block_data(0x33, 0)
                 # f1.close()
-                raise
+                # raise
 
 
 start_time = time.time()
